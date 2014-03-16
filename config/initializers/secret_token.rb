d9db34e0f2e2a5396ffb3be95d021b18c816d226
Anthony <lightweightdevelopment@gmail.com>
2014-03-16 11:54:33 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LibraryAssignment::Application.config.secret_key_base = 'c81f1f39a93e80c4a72d76c7a10c3cb2bdfabdaf8f74b0fb6d79ff2330b3d9c6b700b5fc5f66e2e5e2c94aae2a3e6b6be87304670e89f468aad9ca76f7d3b0c0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LibraryAssignment::Application.config.session_store :cookie_store, key: '_library_assignment_session'
