5782a233a9c7a6025665956d5543b56c981c2cbf
Anthony <lightweightdevelopment@gmail.com>
2014-03-16 14:44:24 +0000

    Added reports templates, added first requirement; better token generator

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,12 +1,16 @@
-# Be sure to restart your server when you modify this file.
+require 'securerandom'
 
-# Your secret key is used for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
 
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-# You can use `rake secret` to generate a secure secret key.
-
-# Make sure your secret_key_base is kept private
-# if you're sharing your code publicly.
-LibraryAssignment::Application.config.secret_key_base = 'c81f1f39a93e80c4a72d76c7a10c3cb2bdfabdaf8f74b0fb6d79ff2330b3d9c6b700b5fc5f66e2e5e2c94aae2a3e6b6be87304670e89f468aad9ca76f7d3b0c0'
+LibraryAssignment::Application.config.secret_key_base = secure_token
